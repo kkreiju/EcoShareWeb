6b02b97065fa482235a6b17c8a3b8a3ed7edea51
Arjay Nino Saguisa <138178348+kkreiju@users.noreply.github.com>
2025-07-29 22:31:32 +0800

    Update layout.tsx

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -18,10 +18,21 @@ export default async function UserLayout({
     redirect('/auth/login')
   }
 
+  const userData = await supabase
+      .from('User')
+                .select('user_id, user_email, user_firstName, user_middleName, user_lastName, user_isVerified, user_ratings, user_transactionCount, user_profileURL')
+                .eq('user_email', data.user.email)
+                .single();
+
+  data.user.user_metadata = {
+    full_name: userData.data?.user_firstName + ' ' + userData.data?.user_lastName,
+    avatar_url: userData.data?.user_profileURL
+  }
+
   return (
     <SidebarProvider>
       <AppSidebar user={{
-        name: data.user.user_metadata?.full_name || data.user.email || 'User',
+        name: data.user.user_metadata?.full_name || 'User',
         email: data.user.email || '',
         avatar: data.user.user_metadata?.avatar_url || '/avatars/default.jpg'
       }} />
